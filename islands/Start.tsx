7e795054e8d0d3f0932f9561524597e7099e3fbc
Benjamín Navarro <benjamin.navarro.m@outlook.cl>
2025-02-04 23:11:36 -0300

    change

--- islands/Start.tsx
+++ islands/Start.tsx
@@ -16,10 +16,10 @@ export default function Start({ ref }: ComponentProps) {
       class={`min-h-[calc(100vh-10vh)] md:min-h-[calc(100vh-12vh)] lg:min-h-[calc(100vh-18vh)] xl:min-h-[calc(100vh-16vh)] flex justify-center items-center w-full p-[12%] mb-10`}
       ref={ref}
     >
+      <div>
       {/**
        * This <div/> avoids the the typing animation to use the full heigth of the screen.
        */}
-      <div>
         <code 
           class={'type text-4xl text-left select-none'}
           style={{
